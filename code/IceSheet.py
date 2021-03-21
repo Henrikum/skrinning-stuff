033e75ab18d9e4d6e5309908d0c490408e342568
Henrik Kockum <henrik.kockum@alfalaval.com>
2021-03-21 20:50:51 +0100

    Created IceSheet() and props.json

---
+++ code/IceSheet.py
@@ -0,0 +1,29 @@
+# IceSheet class
+# Created 2021-03-21
+
+import os
+import json
+
+class IceSheet():
+    """[summary]
+    """
+    def __init__(self, type='lake', thickness=10.0, crystalSize=100.0):
+        """[summary]
+
+        Args:
+            type (str, optional): Type of water that the ice is formed in. Defaults to 'lake'.
+            thickness (float, optional): Initial thickness of icesheet, in mm. Defaults to 10 mm.
+            crystalSize (float, optional): Crystal size in mm. Defaults to 100 mm.
+        """
+        self._iceType = type
+        self._h0 = thickness
+        self._crystSize = crystalSize
+
+        currentDir = os.getcwd()
+        dataFilePath = os.path.join(currentDir, '../data' )
+        dataFilePath = os.path.join(dataFilePath, 'freshWaterProps.json')
+        dataFile = open(dataFilePath, 'r')
+        water = json.load(dataFile)
+        dataFile.close()
+        self._ice = water['solid']
+

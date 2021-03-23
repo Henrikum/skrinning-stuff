12cb390f85b6080eafec63280f75c695f846828b
Henrik Kockum <henrik.kockum@alfalaval.com>
2021-03-23 21:31:16 +0100

    Model now for ice and more informative printouts.

--- code/IceSheet.py
+++ code/IceSheet.py
@@ -10,16 +10,22 @@ import scipy.sparse as ss
 import geomUtils
 
 
+# constants
+EPSILON = 0.95
+SIGMA_SB = 5.67E-8  # W/(m2 K4)
+DT_K = 273.15  # K
+
 class IceSheet():
     """[summary]
     """
+
     def __init__(self, type='lake', thickness=10.0, crystalSize=100.0):
         """[summary]
 
         Args:
             type (str, optional): Type of water that the ice is formed in. Defaults to 'lake'.
-            thickness (float, optional): Initial thickness of icesheet, in mm. Defaults to 10 mm.
-            crystalSize (float, optional): Crystal size in mm. Defaults to 100 mm.
+            thickness (float, optional): Initial thickness of icesheet (mm). Defaults to 10 mm.
+            crystalSize (float, optional): Crystal size (mm). Defaults to 100 mm.
         """
         self._iceType = type
         self._h0 = thickness
@@ -34,8 +40,24 @@ class IceSheet():
         dataFile.close()
         self._ice = water['solid']
 
+
     def getBorderDensity(self):
         return geomUtils.periphery(geom=self.crystType, dims=[self._crystSize])/geomUtils.area(geom=self.crystType, dims=[self._crystSize])
+
+
+    def _u(self, T, THi, TLo):
+        """Returns the non-dimensional temperature u.
+
+        Args:
+            T (float): the temperature (°C or K)
+            THi (float): the high characteristic temperature of the system (°C or K)
+            TLo (float): the low characteristic temperature of the system (°C or K)
+
+        Returns:
+            float: the non-dimensional temperature (---)
+        """
+        return (T - TLo)/(THi - TLo)
+
 
     def setupModel(self, IC=4., BC=0., TEnv=[-4.0, -20.0, -270.]):
         # process
@@ -45,60 +67,50 @@ class IceSheet():
         TAtm = TEnv[1]  # °C
         TSpc = TEnv[2]  # °C
 
-
-
         aAmb = 0.
         aAtm = 0.75
         aSpc = 1 - aAmb - aAtm
 
         # properties
-        lambdaW = 0.56  # W/(m K)
-        rhoW = 999.8  # kg/m3
-        cpW = 4218.  # J/(kg K)
-        # lambdaW = 14.  # W/(m K)
-        # rhoW = 8000.  # kg/m3
-        # cpW = 500.  # J/(kg K)
-        a = lambdaW/rhoW/cpW
-        print(a)
+        lmbda = self._ice['lambda']  # W/(m K)
+        rho = self._ice['rho']  # kg/m3
+        cp = self._ice['cp']  # J/(kg K)
+        a = lmbda/rho/cp
+        print('thermal diffusivity a = {} m2/s'.format(a))
 
         # spatial grid
-        LScale = 0.2  # m
+        LScale = self._h0*0.001  # m
         XScale = 1.0  # ---
         Nx = 50  # number of space steps
         dx = XScale/(Nx - 1)
         x_grid = np.array([i*dx for i in range(Nx)])
-        print(dx)
+        print('spatial stepsize dx = {}'.format(dx))
 
         # temporal grid
         tScale = LScale**2/a  # s
         tChar = tScale  # 3600  # s
-        print(tScale, tScale/tChar)
+        print('timescale tScale = {} s, tScale/tChar = {}'.format(tScale, tScale/tChar))
         YScale = tScale/tChar
-        print(YScale)
+        print('YScale = {}'.format(YScale))
         Ny = 1000  # number of time steps
         dy = YScale/(Ny - 1)
         # dy = dt/1.
         y_grid = np.array([i*dy for i in range(Ny)])
         tSoln = YScale*tChar
-        print(dy, tSoln)
+        print('temporal stepsize dy = {}, solution time tSoln = {} s'.format(dy, tSoln))
 
         # parameters
         # sigma = a*dy/(2.*dx**2)
         sigma = a*(tChar*dy)/(2.*(LScale*dx)**2)
-        print(sigma)
+        print('sigma = {}'.format(sigma))
 
-        epsilon = 0.95
-        sigmaSB = 5.67E-8  # W/(m2 K4)
-        qRad = epsilon*sigmaSB*(np.power(TSurfAve+273.15, 4) - aAmb*np.power(TAmb+273.15, 4) - aAtm*np.power(TAtm+273.15, 4) - aSpc*np.power(TSpc+273.15, 4))
+        qRad = EPSILON*SIGMA_SB*(np.power(TSurfAve+DT_K, 4) - aAmb*np.power(TAmb+DT_K, 4) - aAtm*np.power(TAtm+DT_K, 4) - aSpc*np.power(TSpc+DT_K, 4))
         alphaRad = qRad/(TSurfAve - TAmb)
-        Bi = alphaRad*LScale/lambdaW
-        print(qRad, alphaRad, Bi)
+        Bi = alphaRad*LScale/lmbda
+        print('qRad = {} W/m2, alphaRad = {} W/(m2 K), Bi = {}'.format(qRad, alphaRad, Bi))
 
-        def u(T, T0, TChar):
-            return (T-TChar)/(T0-TChar)
-
-        U0 = u(T0, T0, TAmb)
-        UAmb = u(TAmb, T0, TAmb)
+        U0 = self._u(T0, T0, TAmb)
+        UAmb = self._u(TAmb, T0, TAmb)
 
         # set up matrices
         A = ss.diags([-sigma, 1 + 2*sigma, -sigma], [-1, 0, 1], shape=(Nx, Nx)).toarray()
@@ -119,7 +131,6 @@ class IceSheet():
         b[0] = 4*Bi*dx*sigma*UAmb
 
         # IC
-        # U =  np.array([U0 for i in range(0, Nx)])
         U = np.full(Nx, U0)
 
         return A, B, b, U

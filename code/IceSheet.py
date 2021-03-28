17de04aecf8d747fe91f8f9241425b3abe2cc277
Henrik Kockum <henrik.kockum@alfalaval.com>
2021-03-28 21:50:46 +0200

    Model finalized, ice types and irradiance data to do.

--- code/IceSheet.py
+++ code/IceSheet.py
@@ -27,9 +27,10 @@ class IceSheet():
             thickness (float, optional): Initial thickness of icesheet (mm). Defaults to 10 mm.
             crystalSize (float, optional): Crystal size (mm). Defaults to 100 mm.
         """
+        # TODO: iceType = ['blackIce', 'snowIce', 'seaIce', ...]
         self._iceType = type
-        self._h0 = thickness
-        self._crystSize = crystalSize
+        self._h0 = thickness*0.001  # m
+        self._crystSize = crystalSize*0.001  # m
         self._crystType = 'hex'
 
         currentDir = os.getcwd()
@@ -39,6 +40,15 @@ class IceSheet():
         water = json.load(dataFile)
         dataFile.close()
         self._ice = water['solid']
+
+        # TODO: read from ice property .json
+        # self._I0 = 80.  # W/m2
+        # self._alpha = 1.2  # 1/m
+        self._I0 = 50.  # W/m2
+        self._alpha = 10.  # 1/m
+
+        print('Ice type: {}'.format(self._iceType))
+        print('I0 = {:.3f}, alpha = {:.3f}'.format(self._I0, self._alpha))
 
 
     def getBorderDensity(self):
@@ -59,58 +69,71 @@ class IceSheet():
         return (T - TLo)/(THi - TLo)
 
 
-    def setupModel(self, IC=4., BC=0., TEnv=[-4.0, -20.0, -270.]):
+    def setupModel(self, IC=[-4., 0.], windSpeed=5., aEnv=[0., 0.75, 0.25], TEnv=[-2.0, -20.0, -270.], Nx=51, Ny=101):
         # process
-        T0 = IC  # °C
-        TSurfAve = BC  # °C
         TAmb = TEnv[0]  # °C
         TAtm = TEnv[1]  # °C
         TSpc = TEnv[2]  # °C
+        THi = 0.  # °C
+        TLo = TAmb
+        TSurfAve = np.array(IC).mean()  # °C
 
-        aAmb = 0.
-        aAtm = 0.75
+        aEnv = aEnv/np.array(aEnv).sum()  # rescale if not sum to 1
+        aAmb = aEnv[0]
+        aAtm = aEnv[1]
         aSpc = 1 - aAmb - aAtm
 
         # properties
         lmbda = self._ice['lambda']  # W/(m K)
         rho = self._ice['rho']  # kg/m3
         cp = self._ice['cp']  # J/(kg K)
-        a = lmbda/rho/cp
-        print('thermal diffusivity a = {} m2/s'.format(a))
+        a = lmbda/rho/cp  # m2/s
+        print('thermal diffusivity a = {:.2e} m2/s'.format(a))
 
         # spatial grid
-        LScale = self._h0*0.001  # m
-        XScale = 1.0  # ---
-        Nx = 50  # number of space steps
-        dx = XScale/(Nx - 1)
-        x_grid = np.array([i*dx for i in range(Nx)])
-        print('spatial stepsize dx = {}'.format(dx))
+        zScale = self._h0  # m
+        zStep = zScale/(Nx - 1)
+        dx = zStep/zScale
+        # x_grid = np.array([i*dx for i in range(Nx)])
+        print('spatial stepsize dz = {:.2f} mm'.format(zStep*1000))
 
         # temporal grid
-        tScale = LScale**2/a  # s
-        tChar = tScale  # 3600  # s
-        print('timescale tScale = {} s, tScale/tChar = {}'.format(tScale, tScale/tChar))
-        YScale = tScale/tChar
-        print('YScale = {}'.format(YScale))
-        Ny = 1000  # number of time steps
-        dy = YScale/(Ny - 1)
-        # dy = dt/1.
-        y_grid = np.array([i*dy for i in range(Ny)])
-        tSoln = YScale*tChar
-        print('temporal stepsize dy = {}, solution time tSoln = {} s'.format(dy, tSoln))
+        tScale = zScale**2/a  # s
+        tStep = tScale/(Ny - 1)  # s
+        dy = tStep/tScale
+        # y_grid = np.array([i*dy for i in range(Ny)])
+        print('temporal stepsize dt = {:.2f} s'.format(tStep))
+        print('timescale tScale = {:.1f} s'.format(tScale))
 
         # parameters
-        # sigma = a*dy/(2.*dx**2)
-        sigma = a*(tChar*dy)/(2.*(LScale*dx)**2)
+        # sigma = a*dt/(2.*dz**2) = a*(tScale*dy)/(2.*(LScale*dx)**2) = dy/(2.*dx)
+        # sigma = a*(tScale*dy)/(2.*(LScale*dx)**2)
+        sigma = dy/2./dx**2
         print('sigma = {}'.format(sigma))
 
-        qRad = EPSILON*SIGMA_SB*(np.power(TSurfAve+DT_K, 4) - aAmb*np.power(TAmb+DT_K, 4) - aAtm*np.power(TAtm+DT_K, 4) - aSpc*np.power(TSpc+DT_K, 4))
+        # TODO: use the real surface temperature, but must be done when solving (stepping in time)!
+        qRad = EPSILON*SIGMA_SB*(
+            aAmb*np.power(TAmb+DT_K, 4) 
+            + aAtm*np.power(TAtm+DT_K, 4) 
+            + aSpc*np.power(TSpc+DT_K, 4) 
+            - np.power(TSurfAve+DT_K, 4)
+        )
         alphaRad = qRad/(TSurfAve - TAmb)
-        Bi = alphaRad*LScale/lmbda
-        print('qRad = {} W/m2, alphaRad = {} W/(m2 K), Bi = {}'.format(qRad, alphaRad, Bi))
+        # TODO: alphaConv, use windSpeed to get alphaConv
+        alphaConv = 100.
+        Bi = (alphaRad + alphaConv)*zScale/lmbda
+        print('qRad = {:.0f} W/m2, alphaRad = {:.0f} W/(m2 K), Bi = {:.2e}'.format(qRad, alphaRad, Bi))
 
-        U0 = self._u(T0, T0, TAmb)
-        UAmb = self._u(TAmb, T0, TAmb)
+        # sun irradiance (differentiated, non-dimensionalized)
+        i0 = self._I0/(lmbda/self._h0)/(THi - TLo)
+        alphah = self._alpha*self._h0
+        print('i0 = {:.3f}'.format(i0))
+        print('non-dim irradiance source term at top, bottom = {:.3f}, {:.3f}'.format(
+            self.nonDimIrradSource(0, dx, 1, i0, alphah), self.nonDimIrradSource(Nx, dx, 1, i0, alphah))
+        )
+
+        # U0 = self._u(0., THi, TAmb)  # BC at x = 1: T = 0 °C
+        # UAmb = self._u(TAmb, THi, TAmb)
 
         # set up matrices
         A = ss.diags([-sigma, 1 + 2*sigma, -sigma], [-1, 0, 1], shape=(Nx, Nx)).toarray()
@@ -123,14 +146,22 @@ class IceSheet():
         B[0, 1] = 2*sigma
 
         # BC at x = 1
-        A[Nx-1, Nx-2] = -2*sigma
-        B[Nx-1, Nx-2] = 2*sigma
+        # A[Nx-1, Nx-2] = -2*sigma
+        # B[Nx-1, Nx-2] = 2*sigma
+        # A[Nx-1, Nx-2] = -sigma  # i.e. not different!
+        # B[Nx-1, Nx-2] = sigma  # i.e. not different!
 
-        # boundary condition vector
-        b = np.zeros(len(B))
-        b[0] = 4*Bi*dx*sigma*UAmb
+        # boundary conditions and source term vector
+        # b = np.zeros(len(B))
+        b = np.array([self.nonDimIrradSource(j, dx, dy, i0, alphah) for j in range(Nx)])
+        b[0] = b[0] + 4*sigma*dx*Bi*self._u(TAmb, THi, TLo)
+        b[-1] = b[-1] + 2*sigma*self._u(IC[1], THi, TLo)
 
-        # IC
-        U = np.full(Nx, U0)
+        # IC, linear
+        TInit = np.linspace(IC[0], IC[1], Nx)
+        UInit = self._u(TInit, THi, TLo)
 
-        return A, B, b, U
+        return A, B, b, UInit
+
+    def nonDimIrradSource(self, j, dx, dy, i0, alphah):
+        return i0*alphah*dy*np.exp(-alphah*j*dx)

f2b9bf04cac34270e1d75c1ea572bbe5d223de02
Henrik Kockum <henrik.kockum@alfalaval.com>
2021-03-31 19:33:34 +0200

    Refactored setupModel.

--- code/IceSheet.py
+++ code/IceSheet.py
@@ -69,58 +69,86 @@ class IceSheet():
 
     def setupModel(
         self, 
-        IC=[-4., 0.], 
+        IC=[-2., 0.], 
         windSpeed=5., 
         aEnv=[0., 0.75, 0.25], 
         TEnv=[-2.0, -20.0, -270.], 
         S0=100.,
         Nx=51, 
-        Ny=101
+        Ny=101,
+        isVerbose=False
     ):
-        # process
-        TAmb = TEnv[0]  # °C
-        TAtm = TEnv[1]  # °C
-        TSpc = TEnv[2]  # °C
-        THi = 0.  # °C
-        TLo = TAmb
-        TSurfAve = np.array(IC).mean()  # °C
-
-        aEnv = aEnv/np.array(aEnv).sum()  # rescale if not sum to 1
-        aAmb = aEnv[0]
-        aAtm = aEnv[1]
-        aSpc = 1 - aAmb - aAtm
-
-        I0 = (1 - self._ice['albedo'])*S0  # W/m2
-        print('I0 = {:.3f} W/m2'.format(I0))
 
         # properties
         lmbda = self._pureIce['lambda']  # W/(m K)
         rho = self._pureIce['rho']  # kg/m3
         cp = self._pureIce['cp']  # J/(kg K)
         a = lmbda/rho/cp  # m2/s
-        alpha = self._ice['absorptionCoefficient']
-        print('thermal diffusivity a = {:.2e} m2/s'.format(a))
-        print('absorption coefficient alpha = {:.3f} 1/m'.format(alpha))
+        if isVerbose:
+            print('thermal diffusivity a = {:.2e} m2/s'.format(a))
 
         # spatial grid
         zScale = self._h0  # m
         zStep = zScale/(Nx - 1)
         dx = zStep/zScale
-        # x_grid = np.array([i*dx for i in range(Nx)])
-        print('spatial stepsize dz = {:.2f} mm'.format(zStep*1000))
+        if isVerbose:
+            print('spatial stepsize dz = {:.2f} mm'.format(zStep*1000))
 
         # temporal grid
         tScale = zScale**2/a  # s
         tStep = tScale/(Ny - 1)  # s
         dy = tStep/tScale
-        # y_grid = np.array([i*dy for i in range(Ny)])
-        print('temporal stepsize dt = {:.2f} s'.format(tStep))
-        print('timescale tScale = {:.1f} s'.format(tScale))
+        if isVerbose:
+            print('temporal stepsize dt = {:.2f} s'.format(tStep))
+            print('timescale tScale = {:.1f} s'.format(tScale))
 
         # parameter: sigma = a*dt/(2.*dz**2) = a*(tScale*dy)/(2.*(LScale*dx)**2) = dy/(2.*dx)
         sigma = dy/2./dx**2
-        print('sigma = {}'.format(sigma))
+        if isVerbose:
+            print('sigma = {}'.format(sigma))
 
+        # set up matrices (will not change with time)
+        self._A = ss.diags([-sigma, 1 + 2*sigma, -sigma], [-1, 0, 1], shape=(Nx, Nx)).toarray()
+        self._B = ss.diags([sigma, 1 - 2*sigma, sigma], [-1, 0, 1], shape=(Nx, Nx)).toarray()
+
+        # store useful constants
+        self._Nx = Nx
+        self._dx = dx
+        self._dy = dy
+        self._sigma = sigma
+        self._THi = 0.  # °C
+        self._TLo = TEnv[0]  # TAmb; °C
+
+        # set self._b
+        self.setSourceVector(S0)
+
+        # apply BCs
+        A, B, b = self.applyBCs(IC, aEnv, TEnv)
+
+        # IC, linear
+        UInit = self.setIC(IC)
+
+        return A, B, b, UInit
+
+
+    def setSourceVector(self, S0):
+        I0 = (1 - self._ice['albedo'])*S0  # W/m2
+        # irradiance (differentiated, non-dimensionalized)
+        i0 = I0/(self._pureIce['lambda']/self._h0)/(self._THi - self._TLo)
+        absh = self._ice['absorptionCoefficient']*self._h0
+        self._b = np.array([self.nonDimIrradSource(j, self._dx, self._dy, i0, absh) for j in range(self._Nx)])
+
+
+    def applyBCs(self, IC, aEnv, TEnv):
+        TAmb = TEnv[0]  # °C
+        TAtm = TEnv[1]  # °C
+        TSpc = TEnv[2]  # °C
+        TSurfAve = np.array(IC).mean()  # °C
+        aEnv = aEnv/np.array(aEnv).sum()  # rescale if not sum to 1
+        aAmb = aEnv[0]
+        aAtm = aEnv[1]
+        aSpc = 1 - aAmb - aAtm
+        
         # TODO: use the real surface temperature, but must be done when solving (stepping in time)!
         qRad = EPSILON*SIGMA_SB*(
             aAmb*np.power(TAmb+DT_K, 4) 
@@ -131,47 +159,37 @@ class IceSheet():
         alphaRad = qRad/(TSurfAve - TAmb)
         # TODO: alphaConv, use windSpeed to get alphaConv
         alphaConv = 100.
-        Bi = (alphaRad + alphaConv)*zScale/lmbda
-        print('qRad = {:.0f} W/m2, alphaRad = {:.0f} W/(m2 K), Bi = {:.2e}'.format(qRad, alphaRad, Bi))
+        Bi = (alphaRad + alphaConv)*self._h0/self._pureIce['lambda']
+        # print('qRad = {:.0f} W/m2, alphaRad = {:.0f} W/(m2 K), Bi = {:.2e}'.format(qRad, alphaRad, Bi))
 
-        # sun irradiance (differentiated, non-dimensionalized)
-        i0 = I0/(lmbda/self._h0)/(THi - TLo)
-        alphah = alpha*self._h0
-        print('i0 = {:.3f}'.format(i0))
-        print('non-dim irradiance source term at top, bottom = {:.3f}, {:.3f}'.format(
-            self.nonDimIrradSource(0, dx, 1, i0, alphah), self.nonDimIrradSource(Nx, dx, 1, i0, alphah))
-        )
+        dx = self._dx
+        sigma = self._sigma
 
-        # U0 = self._u(0., THi, TAmb)  # BC at x = 1: T = 0 °C
-        # UAmb = self._u(TAmb, THi, TAmb)
-
-        # set up matrices
-        A = ss.diags([-sigma, 1 + 2*sigma, -sigma], [-1, 0, 1], shape=(Nx, Nx)).toarray()
-        B = ss.diags([sigma, 1 - 2*sigma, sigma], [-1, 0, 1], shape=(Nx, Nx)).toarray()
+        A = self._A.copy()
+        B = self._B.copy()
+        b = self._b.copy()
 
         # BC at x = 0
         A[0, 0] = 1 + 2*(1 + Bi*dx)*sigma
         A[0, 1] = -2*sigma
         B[0, 0] = 1 - 2*(1 + Bi*dx)*sigma
         B[0, 1] = 2*sigma
-
+        
         # BC at x = 1
-        # A[Nx-1, Nx-2] = -2*sigma
-        # B[Nx-1, Nx-2] = 2*sigma
-        # A[Nx-1, Nx-2] = -sigma  # i.e. not different!
-        # B[Nx-1, Nx-2] = sigma  # i.e. not different!
+        # does not change A, B
 
-        # boundary conditions and source term vector
-        # b = np.zeros(len(B))
-        b = np.array([self.nonDimIrradSource(j, dx, dy, i0, alphah) for j in range(Nx)])
-        b[0] = b[0] + 4*sigma*dx*Bi*self._u(TAmb, THi, TLo)
-        b[-1] = b[-1] + 2*sigma*self._u(IC[1], THi, TLo)
+        # boundary conditions in source term vector
+        b[0] = b[0] + 4*sigma*dx*Bi*self._u(TAmb, self._THi, self._TLo)
+        b[-1] = b[-1] + 2*sigma*self._u(IC[-1], self._THi, self._TLo)
 
-        # IC, linear
-        TInit = np.linspace(IC[0], IC[1], Nx)
-        UInit = self._u(TInit, THi, TLo)
+        return A, B, b
 
-        return A, B, b, UInit
+
+    def setIC(self, IC):
+        TInit = np.linspace(IC[0], IC[1], self._Nx)
+        UInit = self._u(TInit, self._THi, self._TLo)
+        return UInit
+
 
     def nonDimIrradSource(self, j, dx, dy, i0, alphah):
         return i0*alphah*dy*np.exp(-alphah*j*dx)
